028022d225951c26e0fd4d07e335920ab1a6c9d1
Julien <newdevcreator@gmail.com>
2024-02-12 19:59:31 +0100

    FEAT: quick UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import Gradient from "./components/Gradient";
+
 function App() {
-  return <div></div>
+  return (
+    <div className="relative text-slate-100 max-w-xl mx-auto mt-20 p-4 border border-slate-400 flex">
+      <div className="w-1/2 p-4 pr-8">
+        <h1 className="text-center text-xl">Gradient Generator</h1>
+        <p className="text-center mb-6">Bring style to your apps.</p>
+
+        <p className="mb-1">Colors, min 2, max 5.</p>
+        <div className="flex mb-2">
+          <input type="color" />
+          <input type="color" />
+        </div>
+        <div className="mb-5">
+          <button>+</button>
+          <button>-</button>
+        </div>
+
+        <p>Pick and change a Color's position</p>
+        {/* SelectColor */}
+
+        <p>Color's position</p>
+        {/* RangeColor */}
+
+        <p>Gradient global angle</p>
+        {/* RangeAngle */}
+
+        {/* OpenModalBtn */}
+      </div>
+      <Gradient />
+    </div>
+  );
 }
 
-export default App
+export default App;
