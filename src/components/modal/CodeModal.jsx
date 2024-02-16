9ee676ac2db7275649bb64cf24e9d52765bc8110
Julien <newdevcreator@gmail.com>
2024-02-16 21:32:36 +0100

    FEAT: close modal

--- src/components/modal/CodeModal.jsx
+++ src/components/modal/CodeModal.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-export default function CodeModal() {
+export default function CodeModal({ closeModal }) {
   return (
-    <div className="fixed z-10 top-0 left-0 w-full h-full bg-gray-800/95 flex justify-center items-center">
-      <div className="max-w-[500px] rounded p-7 bg-gray-50">
+    <div
+      onClick={closeModal}
+      className="fixed z-10 top-0 left-0 w-full h-full bg-gray-800/95 flex justify-center items-center"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-[500px] rounded p-7 bg-gray-50"
+      >
         <div className="flex items-center mb-5">
           <p className="font-semibold text-gray-950 mr-6">Here is your code!</p>
           <button className="ml-auto mr-2 text-sm bg-blue-600 text-white hover:bg-blue-700 py-1 px-3 rounded">
             Copy
           </button>
-          <button className="text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded">
+          <button
+            onClick={closeModal}
+            className="text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded"
+          >
             Close
           </button>
         </div>
